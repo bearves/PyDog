6b01761c735dfe39d32fac538cc69b7e0319fd14
unknown <qiaosun1234@hotmail.com>
2022-11-05 19:00:48 +0800

    clear unused codes in RobotSim.py

--- RobotSim.py
+++ RobotSim.py
@@ -28,7 +28,6 @@ jnt_num = pb.getNumJoints(dog)
 for i in range(jnt_num):
     info = pb.getJointInfo(dog, i)
     joint_type = info[2]
-    print(info[0], info[1], info[12])
     # unlock joints
     if joint_type == pb.JOINT_REVOLUTE:
         pb.setJointMotorControl2(dog, i, pb.VELOCITY_CONTROL, force=0)
@@ -39,18 +38,6 @@ act_jnt_id = [1,  3,  4,  # FR
               6,  8,  9,  # FL
               11, 13, 14,  # RR
               16, 18, 19]  # RL
-toe_link_id = [5, 10, 15, 20]
-
-jnt_act_pos = np.zeros(act_jnt_num)
-jnt_act_vel = np.zeros(act_jnt_num)
-body_pos = np.zeros(3)
-body_orn = np.array([0, 0, 0, 1])
-body_vel = np.zeros(3)
-body_angvel = np.zeros(3)
-
-gait_controller = RobotGaitControl.QuadGaitController(use_mpc=True)
-feedbacks = RobotGaitControl.QuadControlInput()
-user_cmd = RobotGaitControl.QuadRobotCommands()
 
 jnt_ref_pos0 = np.array([-0.0, 0.75, -1.35,
                         -0.0, 0.75, -1.35,
@@ -58,12 +45,15 @@ jnt_ref_pos0 = np.array([-0.0, 0.75, -1.35,
                         -0.0, 0.75, -1.35])
 jnt_ref_vel0 = np.array([0, 0, 0, 0, 0, 0,
                         0, 0, 0, 0, 0, 0])
-jnt_ref_pos = jnt_ref_pos0.copy()
-jnt_ref_vel = jnt_ref_vel0.copy()
 
 count = 0
-in_gait_start_time = 6.0
+in_gait_start_time = 3.0
 in_gait = False
+
+# load controller
+gait_controller = RobotGaitControl.QuadGaitController(use_mpc=True)
+feedbacks = RobotGaitControl.QuadControlInput()
+user_cmd = RobotGaitControl.QuadRobotCommands()
 
 # log
 log_size = 10000
@@ -76,27 +66,20 @@ while 1:
     count += 1
     time_now = (count-1) * time_step
 
+    # get robot body states
+    base_pos = pb.getBasePositionAndOrientation(dog)
+    base_vel = pb.getBaseVelocity(dog)
+    feedbacks.time_now = time_now
+    feedbacks.body_pos = np.array(base_pos[0])
+    feedbacks.body_vel = np.array(base_vel[0])
+    feedbacks.body_orn = np.array(base_pos[1])
+    feedbacks.body_angvel = np.array(base_vel[1])
+
     # get joint states
     for i in range(act_jnt_num):
         jnt_state = pb.getJointState(dog, act_jnt_id[i])
-        jnt_act_pos[i] = jnt_state[0]
-        jnt_act_vel[i] = jnt_state[1]
-
-    # get robot body states
-    ret_pos = pb.getBasePositionAndOrientation(dog)
-    body_pos = np.array(ret_pos[0])
-    body_orn = np.array(ret_pos[1])
-    ret_vel = pb.getBaseVelocity(dog)
-    body_vel = np.array(ret_vel[0])
-    body_angvel = np.array(ret_vel[1])
-
-    feedbacks.time_now = time_now
-    feedbacks.body_pos = body_pos.copy()
-    feedbacks.body_vel = body_vel.copy()
-    feedbacks.body_orn = body_orn.copy()
-    feedbacks.body_angvel = body_angvel.copy()
-    feedbacks.jnt_act_pos = jnt_act_pos.copy()
-    feedbacks.jnt_act_vel = jnt_act_vel.copy()
+        feedbacks.jnt_act_pos[i] = jnt_state[0]
+        feedbacks.jnt_act_vel[i] = jnt_state[1]
 
     if not in_gait and time_now > in_gait_start_time:
         in_gait = True
@@ -108,19 +91,22 @@ while 1:
         jnt_ref_trq = output.joint_tgt_trq.copy()
     else:
         kp, kd = 120, 2
-        pos_err = jnt_ref_pos - jnt_act_pos
-        vel_err = jnt_ref_vel - jnt_act_vel
+        pos_err = jnt_ref_pos0 - feedbacks.jnt_act_pos
+        vel_err = jnt_ref_vel0 - feedbacks.jnt_act_vel
         jnt_ref_trq = kp * pos_err + kd * vel_err
 
     max_trq = 50
     jnt_ref_trq = np.clip(jnt_ref_trq, -max_trq, max_trq)
 
+    # set commands
     for i in range(act_jnt_num):
         pb.setJointMotorControl2(
             dog, act_jnt_id[i], pb.TORQUE_CONTROL, force=jnt_ref_trq[i])
+    
+    # proceed simulation 
+    pb.stepSimulation()
 
     # keyboard event handler
-    pb.stepSimulation()
     keys = pb.getKeyboardEvents()
 
     if ord('t') in keys and keys[ord('t')] & pb.KEY_WAS_TRIGGERED:
@@ -152,10 +138,10 @@ while 1:
     time_line[count % log_size] = time_now
     tip_pos_wcs_log[count % log_size, :] = gait_controller.tip_ref_pos
     qj_log[count % log_size, :] = gait_controller.jnt_ref_pos_wbic
-    jnt_ref_pos_log[count % log_size, :] = jnt_ref_pos
+    jnt_ref_pos_log[count % log_size, :] = gait_controller.jnt_ref_pos_wbic
 
     #time.sleep(time_step)
-    cam_pos = body_pos.copy()
+    cam_pos = feedbacks.body_pos.copy()
     cam_pos[2] = 0.2
     pb.resetDebugVisualizerCamera(1, 180, -30, cam_pos)
 
